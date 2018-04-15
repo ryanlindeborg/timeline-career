1fe355a9be73ff17ec106adbad967651f788fce0
Ryan Lindeborg <lindeborgryan@gmail.com>
2018-04-15 10:49:56 -0400

    Fixed helpers file

--- helpers.py
+++ helpers.py
@@ -1,9 +1,37 @@
 from flask import redirect, render_template, request, session, url_for
 from functools import wraps
-from cs50 import SQL
+# from cs50 import SQL
+
+import os
+import sqlalchemy
+
+class SQL(object):
+    def __init__(self, url):
+        try:
+            self.engine = sqlalchemy.create_engine(url)
+        except Exception as e:
+            raise RuntimeError(e)
+    def execute(self, text, *multiparams, **params):
+        try:
+            statement = sqlalchemy.text(text).bindparams(*multiparams, **params)
+            result = self.engine.execute(str(statement.compile(compile_kwargs={"literal_binds": True})))
+            # SELECT
+            if result.returns_rows:
+                rows = result.fetchall()
+                return [dict(row) for row in rows]
+            # INSERT
+            elif result.lastrowid is not None:
+                return result.lastrowid
+            # DELETE, UPDATE
+            else:
+                return result.rowcount
+        except sqlalchemy.exc.IntegrityError:
+            return None
+        except Exception as e:
+            raise RuntimeError(e)
 
 # configure CS50 Library to use SQLite database
-db = SQL("sqlite:///timeline.db")
+db = SQL(os.environ["DATABASE_URL"])
 
 def apology(text):
     """Renders message as an apology to user."""
